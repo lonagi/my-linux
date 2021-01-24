835cf5a22d2ecff4e4388e93863618b9239f9367
Sasha <40319171+lonagi@users.noreply.github.com>
2021-01-24 13:03:54 +0200

    install google chrome

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 echo "Remove libreoffice garbage"
 sudo apt-get remove -y libreoffice-draw libreoffice-math libreoffice-base
+
+echo "Setup Google Chrome"
+sudo apt install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome*.deb
+# apt-get install -f.
+rm -f ./google-chrome*.deb
