0c2e8ffdef68297ad2eda9a9fe1121854f2f634c
Sasha <40319171+lonagi@users.noreply.github.com>
2021-02-05 19:59:10 +0200

    Install sublime

--- install.sh
+++ install.sh
@@ -114,6 +114,12 @@ sudo apt install -y winff
 echo "Install Blender"
 sudo apt install -y blender
 
+echo "Install Sublime text"
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install -y sublime-text
+
 echo "============="
 echo "Do it yourself"
 echo "Install NordVPN"
