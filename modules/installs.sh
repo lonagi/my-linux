9612efef81145259bb592ab81cf39e7d06f3d517
Sasha <lonagi22@gmail.com>
2021-02-10 17:31:50 +0200

    Create installs.sh

---
+++ modules/installs.sh
@@ -0,0 +1,31 @@
+echo "Install some CLI, CMake, Impress, mousepad, Discord, Snap"
+apt install -y git curl aptitude htop neofetch lolcat build-essential cmake qtwebengine5-dev libreoffice-impress mousepad snapd python3-pip python3-opencv ffmpeg winff blender gparted obs-studio
+snap install core zoom-client
+
+echo "Install Discord"
+sudo snap install discord
+snap connect discord:system-observe
+
+echo "Bluetooth drivers for JBL"
+# apt install -y pulseaudio pulseaudio-module-bluetooth pavucontrol bluez-firmware blueman
+# killall pulseaudio
+apt install -y pavucontrol bluez-firmware blueman
+pulseaudio -k
+pulseaudio --start
+
+echo "Setup Google Chrome"
+#wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+#sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+#deb http://dl.google.com/linux/chrome/deb/ stable main
+curl -sL https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+apt install -y google-chrome-stable
+
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# apt install libxss1 libappindicator1 libindicator7
+# apt install ./google-chrome*.deb
+# # apt-get install -f.
+# rm -f ./google-chrome*.deb
+
+#autoload zkbd && zkbd
+cp -r ./mymotd /etc/motd
+echo 'cat /etc/motd' >> ~/.zsh/.zshrc
