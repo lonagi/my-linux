e63ba6eab006671678a15cbd17d2992485135925
Sasha <lonagi22@gmail.com>
2021-02-10 17:31:47 +0200

    Update deleter.sh

--- modules/deleter.sh
+++ modules/deleter.sh
@@ -1,18 +1,4 @@
-echo "Remove libreoffice garbage"
-sudo apt-get remove -y libreoffice-draw libreoffice-math libreoffice-base
-
-echo "Remove Firefox"
-sudo apt-get -y remove firefox-esr
-sudo apt-get autoremove firefox-esr
-sudo apt-get purge firefox-esr
-sudo rm -Rf /home/fnvg/.cache/mozilla/
-sudo rm -Rf /home/fnvg/.mozilla/
-
-echo "Remove garbage soft"
-sudo apt purge -y okular
-sudo apt purge -y konqueror
-sudo apt purge -y kaddressbook 
-sudo apt purge -y akregator
-
-echo "Autoremove"
-sudo apt autoremove -y
+echo "Remove garbage"
+apt purge -y libreoffice-draw libreoffice-math libreoffice-base firefox-esr okular konqueror kaddressbook akregator
+rm -rf $HOME/.cache/mozilla/ $HOME/.mozilla/
+apt autoremove -y
