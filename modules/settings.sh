3c56617a53e6b09c98c73f18e72c16bdb6a284e7
Sasha <40319171+lonagi@users.noreply.github.com>
2021-02-15 10:41:27 +0200

    add kali repo

--- modules/settings.sh
+++ modules/settings.sh
@@ -1,3 +1,13 @@
 echo "SUDO fix problems"
 chown root:root /etc/sudoers.d
 chmod 755 /etc/sudoers.d
+
+echo "Add kali repo"
+cp etc/kali.list /etc/apt/sources.list.d/
+apt clean
+apt update
+apt upgrade
+apt dist-upgrade
+git clone https://github.com/LionSec/katoolin.git
+mv katoolin/katoolin.py /usr/bin/katoolin
+chmod +x /usr/bin/katoolin
